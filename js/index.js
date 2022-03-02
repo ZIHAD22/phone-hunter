af079fe8767b1b37c48a112ff803b27e25df1fd2
ZIHAD22 <mdzihad2244@gmail.com>
2022-03-02 16:03:30 +0600

    data display dinamic

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const fetchData = async () => {
+  const phoneBrand = ['oppo', 'iphone', 'samsung']
+  const selectBrand = Math.floor(Math.random() * phoneBrand.length)
+
+  const url = `https://openapi.programming-hero.com/api/phones?search=${phoneBrand[selectBrand]}`
+
+  const data = await fetch(url)
+  const realData = await data.json()
+
+  displayData(realData)
+  console.log(realData)
+}
+
+fetchData()
+
+const displayData = (realData) => {
+  const card = document.getElementById('search-result')
+  const phoneData = realData.data.map((phone) => {
+    const div = document.createElement('div')
+    div.classList.add('col')
+    div.innerHTML = `
+        <div class="card">
+            <img class="m-2" src="${phone.image}" class="card-img-top" alt="..." />
+            <div class="card-body">
+            <h3 class="card-title">Name: ${phone.phone_name}</h3>
+                <h4>Brand:${phone.brand}</h4>
+            </div>
+            <a class="btn btn-success w-50 ms-3 mb-4" href="">Details</a>
+        </div>`
+
+    card.appendChild(div)
+    console.log(card)
+  })
+}
