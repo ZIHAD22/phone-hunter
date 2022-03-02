9e1a8b622a87df88db9703d520f1d5828207880c
ZIHAD22 <mdzihad2244@gmail.com>
2022-03-02 22:45:34 +0600

    all done

--- js/index.js
+++ js/index.js
@@ -23,24 +23,67 @@ const fetchData = async () => {
 fetchData()
 
 // display Data By Default
-const displayData = (realData) => {
-  const card = document.getElementById('search-result')
-  const phoneData = realData.data.map((phone) => {
+const displayData = async (realData, isShowShort) => {
+  if (!realData.status) {
+    const error = document.getElementById('error-msg')
+    while (error.hasChildNodes()) {
+      error.removeChild(error.firstChild)
+    }
     const div = document.createElement('div')
-    div.classList.add('col')
     div.innerHTML = `
-        <div class="card">
-            <img class="m-2" src="${phone.image}" class="card-img-top" alt="..." />
-            <div class="card-body">
-            <h3 class="card-title">Name: ${phone.phone_name}</h3>
-                <h4>Brand:${phone.brand}</h4>
-            </div>
-            <a onclick="expanDetails('${phone.slug}')" class="btn btn-success w-50 ms-3 mb-4" href="#phone-details">Details</a>
-        </div>`
+    
+    <h1 class="text-center my-5">No Result Found</h1>
+    
+    `
 
-    card.appendChild(div)
-    // console.log(card)
-  })
+    error.appendChild(div)
+  } else {
+    const error = document.getElementById('error-msg')
+    while (error.hasChildNodes()) {
+      error.removeChild(error.firstChild)
+    }
+    const card = document.getElementById('search-result')
+    if (isShowShort) {
+      const phone = realData.data
+      for (let i = 0; i < realData.data.length; i++) {
+        if (i == 20) {
+          const showMore = document.getElementById('show-more')
+          showMore.classList.remove('d-none')
+          break
+        } else {
+          const div = document.createElement('div')
+          div.classList.add('col')
+          div.innerHTML = `
+              <div class="card">
+                  <img class="m-2" src="${phone[i]?.image}" class="card-img-top" alt="..." />
+                  <div class="card-body">
+                  <h3 class="card-title">Name: ${phone[i]?.phone_name}</h3>
+                      <h4>Brand:${phone[i]?.brand}</h4>
+                  </div>
+                  <a onclick="expanDetails('${phone[i]?.slug}')" class="btn btn-success w-50 ms-3 mb-4" href="#phone-details">Details</a>
+              </div>`
+
+          card.appendChild(div)
+        }
+      }
+    } else {
+      const phoneData = realData.data.map((phone) => {
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `
+            <div class="card">
+                <img class="m-2" src="${phone.image}" class="card-img-top" alt="..." />
+                <div class="card-body">
+                <h3 class="card-title">Name: ${phone.phone_name}</h3>
+                    <h4>Brand:${phone.brand}</h4>
+                </div>
+                <a onclick="expanDetails('${phone.slug}')" class="btn btn-success w-50 ms-3 mb-4" href="#phone-details">Details</a>
+            </div>`
+        card.appendChild(div)
+        // console.log(card)
+      })
+    }
+  }
 }
 
 // Search Data By user searching Key Word
@@ -48,14 +91,21 @@ const displayData = (realData) => {
 const searchData = async () => {
   const searchInput = document.getElementById('search-input')
   let searchValue = searchInput.value
-  const individualData = await fetching(searchValue, false)
+  let storedSearchValue = searchValue
+  console.log(storedSearchValue)
+  const individualData = await fetching(storedSearchValue, false)
   console.log(individualData)
   const preSearchData = document.getElementById('search-result')
   while (preSearchData.hasChildNodes()) {
     preSearchData.removeChild(preSearchData.firstChild)
   }
-  displayData(individualData)
+  displayData(individualData, true)
   searchInput.value = ''
+}
+
+// all data show
+const showAllData = () => {
+  searchData()
 }
 
 // Phone Details function
